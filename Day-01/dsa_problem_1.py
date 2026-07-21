eaaa76e3a8c70e75fb031a52f66e1849137bec76
achyuthastroo3-blip <achyuthastroo3@gmail.com>
2026-07-21 12:55:24 +0530

    Solved 2 DSA problems

--- Day-01/dsa_problem_1.py
+++ Day-01/dsa_problem_1.py
@@ -1,3 +1,7 @@
-print(
-    "Hello, World!"
-)
+arr=list(map(int,input().split())) # creating a list of integers from user input
+count=0 # The problem want to find the number of elements greater than or equal to 10. So we will initialize a counter variable count to 0. 
+for i in range(4):
+    if arr[i]>=10:
+        count+=1
+print(count)
+
---
+++ Day-01/dsa_problem_2.py
@@ -0,0 +1,7 @@
+import math
+a=int(input())
+for i in range(a):
+    b=int(input())
+    print(math.factorial(b))
+
+# They want to find the factorial of a number. So we will use the math library in python which has a built-in function called factorial() that takes an integer as input and returns its factorial. We will take the input from the user and pass it to the factorial() function and print the result. 
